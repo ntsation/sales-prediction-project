a4a13f8c8ddce77b8885e581f58093ffbeff2346
Nathan Souza <naathanmarinho11@gmail.com>
2025-02-15 12:24:45 -0300

    fix: ruff format

--- api/main.py
+++ api/main.py
@@ -3,18 +3,22 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 
-model = joblib.load('models/sales_predictor.pkl')
+model = joblib.load("models/sales_predictor.pkl")
 
 app = FastAPI()
+
 
 class SalesRequest(BaseModel):
     features: list
 
-@app.post('/predict')
+
+@app.post("/predict")
 def predict_salves(request: SalesRequest):
     prediction = model.predict(np.array([request.features]))[0]
-    return {'predicted_sales': prediction}
+    return {"predicted_sales": prediction}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
